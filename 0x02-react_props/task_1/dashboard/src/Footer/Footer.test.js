8fe26d572fc540ee957c51c1e6578329a4296438
omarnate1 <111249086+omarnate1@users.noreply.github.com>
2023-07-31 20:30:08 +0100

    Update Footer.test.js

--- 0x02-react_props/task_1/dashboard/src/Footer/Footer.test.js
+++ 0x02-react_props/task_1/dashboard/src/Footer/Footer.test.js
@@ -1,8 +1,15 @@
-.App-footer {
-  border-top: 3px solid #F2003A;
-  text-align: center;
-  font-style: italic;
-  font-size: 20;
-  font-family: 'Poppins', sans-serif;
+import { shallow } from "enzyme";
+import React from "react";
+import Footer from "./Footer";
+import { getFullYear, getFooterCopy } from "../utils/utils";
 
-}
+describe("Footer test", () => {
+  it("should render without crashing", () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.exists()).toEqual(true);
+  });
+  it("should render the text Copyright", () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.text()).toEqual(`Copyright ${getFullYear()} - ${getFooterCopy()}`);
+  });
+});
