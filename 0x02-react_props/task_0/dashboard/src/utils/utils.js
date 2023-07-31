5fceb5d55b8cdbfbe553be1764cc1b092b1a8198
omarnate1 <111249086+omarnate1@users.noreply.github.com>
2023-07-31 19:41:34 +0100

    Update utils.js

--- 0x02-react_props/task_0/dashboard/src/utils/utils.js
+++ 0x02-react_props/task_0/dashboard/src/utils/utils.js
@@ -4,10 +4,4 @@ export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holbe
 
 export const getLatestNotification = () => {
   return "<strong>Urgent requirement</strong> - complete by EOD";
-};export const getFullYear = () => new Date().getFullYear();
-
-export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
-
-export const getLatestNotification = () => {
-  return "<strong>Urgent requirement</strong> - complete by EOD";
 };
