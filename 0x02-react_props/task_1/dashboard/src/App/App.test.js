9cf02dbf49b21bb9aab51e09d6fb95df6f147afe
omarnate1 <111249086+omarnate1@users.noreply.github.com>
2023-07-31 20:28:30 +0100

    Update App.test.js

--- 0x02-react_props/task_1/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_1/dashboard/src/App/App.test.js
@@ -1,5 +1,9 @@
 import React from "react";
 import App from "./App";
+import Login from "../Login/Login";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Notifications from "../Notifications/Notifications";
 import { shallow } from "enzyme";
 
 describe("App tests", () => {
@@ -8,19 +12,24 @@ describe("App tests", () => {
 
     expect(component).toBeDefined();
   });
-  // it('should render a div with the class App-header', () => {
-  // 	const component = shallow(<App />);
+  it("should render Notifications component", () => {
+    const component = shallow(<App />);
 
-  // 	expect(component.find('.App-header')).toBeDefined();
-  // });
-  // it('should render a div with the class App-body', () => {
-  // 	const component = shallow(<App />);
+    expect(component.contains(<Notifications />)).toBe(true);
+  });
+  it("should render Header component", () => {
+    const component = shallow(<App />);
 
-  // 	expect(component.find('.App-body')).toBeDefined();
-  // });
-  // it('should render a div with the class App-footer', () => {
-  // 	const component = shallow(<App />);
+    expect(component.contains(<Header />)).toBe(true);
+  });
+  it("should render Login Component", () => {
+    const component = shallow(<App />);
 
-  // 	expect(component.find('.App-footer')).toBeDefined();
-  // });
+    expect(component.contains(<Login />)).toBe(true);
+  });
+  it("should render Footer component", () => {
+    const component = shallow(<App />);
+
+    expect(component.contains(<Footer />)).toBe(true);
+  });
 });
