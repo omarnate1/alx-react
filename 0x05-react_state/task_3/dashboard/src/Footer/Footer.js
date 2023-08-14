0d15101792c7dbf9493dee038976d56fb59a2976
omarnate1 <111249086+omarnate1@users.noreply.github.com>
2023-08-14 18:40:45 +0100

    Updated Footer.js

--- 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
+++ 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,14 +1,35 @@
 import React from "react";
-import "./Footer.css";
 import { getFullYear, getFooterCopy } from "../utils/utils";
+import { StyleSheet, css } from "aphrodite";
+import { AppContext } from "../App/AppContext";
+
+
+const styles = StyleSheet.create({
+  paragraph: {
+    textAlign: 'center',
+    fontSize: '1.2rem'
+  },
+  copyright: {
+    marginTop: '40px'
+  }
+})
 
 function Footer() {
   return (
-    <>
-      <div className="App-footer">
-        Copyright {getFullYear()} - {getFooterCopy()}
-      </div>
-    </>
+    <AppContext.Consumer>
+      {(context) => (
+        <footer className='footer'>
+          <p className={css(styles.copyright)}>
+            Copyright {getFullYear()} - {getFooterCopy(true)}
+          </p>
+          {context.user.isLoggedIn && (
+            <p className={css(styles.paragraph)}>
+              <a>Contact us</a>
+            </p>
+          )}
+        </footer>
+      )}
+    </AppContext.Consumer>
   );
 }
 
