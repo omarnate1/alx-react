1b9359876542641b84cb57b40192cbc5f6220783
omarnate1 <111249086+omarnate1@users.noreply.github.com>
2023-08-22 21:23:32 +0100

    async action creators

--- 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
@@ -1,4 +1,5 @@
-import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from './uiActionTypes';
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS, LOGIN_FAILURE } from './uiActionTypes';
 
 export const login = (email, password) => {
   return { 
@@ -29,3 +30,21 @@ export const hideNotificationDrawer = () => {
 };
 
 export const boundhideNotificationDrawer = () => dispatch(hideNotificationDrawer());
+
+export const loginSuccess = () => {
+  return { type: LOGIN_SUCCESS }
+}
+
+export const loginFailure = () => {
+  return { type: LOGIN_FAILURE }
+}
+
+export const loginRequest = (email, password) => {
+  return function (dispatch) {
+    dispatch(login(email, password));
+    return fetch('http://localhost:3000/login-success.json')
+      .then((response) => response.json())
+      .then((json) => dispatch(loginSuccess()))
+      .catch((err) => dispatch(loginFailure()))
+  };
+}
