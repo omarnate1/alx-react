f56d65f125f35a2c54c71491bcbffbab741dccf3
omarnate1 <111249086+omarnate1@users.noreply.github.com>
2023-08-28 23:58:54 +0100

    task done

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
@@ -1,35 +1,26 @@
 import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_TYPE_FILTER } from '../actions/notificationActionTypes'
+import { notificationsNormalizer } from '../schema/notifications'
+import immutable, { setIn } from 'immutable';
 
+const { Map, setIn } = immutable
 
 export const initialState = {
     notifications: [],
     filter: 'DEFAULT'
 }
 
-export const notificationReducer = (state = initialState, action) => {
+export const notificationReducer = (state = Map(initialState), action) => {
     switch(action.type) {
         case FETCH_NOTIFICATIONS_SUCCESS:
-            return {
-                ...state,
-                notifications: action.data.map((not) => {
-                    return { ...not, isRead: false };
-                })
-            };
+            const data = notificationsNormalizer(action.data);
+            Object.keys(data.notifications).map((key) => {
+                data.notifications[key].isRead = false;
+            })
+            return state.merge(data)
         case MARK_AS_READ:
-            return {
-                ...state,
-                notifications: state.notifications.map((not) => {
-                    if (action.index === not.id) {
-                        return { ...not, isRead: true }
-                    }
-                    return { ...not }
-                })
-            };
+            return setIn(state, ['notifications', String(action.index), isRead], true);
         case SET_TYPE_FILTER:
-            return {
-                ...state,
-                filter: action.filter
-            };
+            return setIn('filter', action.filter)
         default:
             break;
     }
