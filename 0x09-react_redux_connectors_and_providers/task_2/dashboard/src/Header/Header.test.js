832a53f10c38ee8f4d3536f3dd81317b5da7d0e3
omarnate1 <111249086+omarnate1@users.noreply.github.com>
2023-09-05 00:20:14 +0100

    connectors and providers

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Header/Header.test.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Header/Header.test.js
@@ -1,23 +1,45 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from "react";
+import { shallow, mount } from "enzyme";
 import Header from "./Header";
-import { shallow } from "enzyme";
-import { StyleSheetTestUtils } from "aphrodite";
+import { AppContext } from '../App/AppContext';
+import { StyleSheetTestUtils } from 'aphrodite';
 
-beforeEach(() => {
-  StyleSheetTestUtils.suppressStyleInjection();
-});
-afterEach(() => {
-  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});
+StyleSheetTestUtils.suppressStyleInjection();
 
-describe("Header", () => {
-  it("render without crashing", () => {
-    const wrapper = shallow(<Header />);
-    expect(wrapper.exists()).toEqual(true);
-  });
-  it("should render a h1", () => {
-    const wrapper = shallow(<Header />);
-    expect(wrapper.exists("img")).toEqual(true);
-    expect(wrapper.containsMatchingElement(<h1>School dashboard</h1>)).toEqual(true);
-  });
-});
+describe("<Header />", () => {
+  it('renders a <Header /> component', () => {
+    const wrapper = shallow(<AppContext.Provider><Header /></AppContext.Provider>);
+    expect(wrapper).toHaveLength(1);
+	});
+
+  it('mounts a <Header /> component and checks contents', () => {
+    const value = { user: {email: '', password: '', isLoggedIn: false}, logOut: () => {} }
+    const wrapper = mount(<AppContext.Provider value={value}><Header /></AppContext.Provider>);
+    expect(wrapper.find('header h1')).toHaveLength(1);
+    expect(wrapper.find('header img')).toHaveLength(1);
+	});
+
+  it('mounts a <Header /> component with default context and verifies that logoutSection is not created', () => {
+    const value = { user: { email: '', password: '', isLoggedIn: false }, logOut: () => {} }
+    const wrapper = mount(<AppContext.Provider value={value}><Header /></AppContext.Provider>);
+    expect(wrapper.find('#logoutSection')).toHaveLength(0);
+	});
+
+  it('mounts a <Header /> component with user defined and verifies that logoutSection is created', () => {
+    const value = { user: { email: 'thedude@aim.com', password: 'thedudeabides', isLoggedIn: true }, logOut: () => {} }
+    const wrapper = mount(<AppContext.Provider value={value}><Header /></AppContext.Provider>);
+    expect(wrapper.find('#logoutSection')).toHaveLength(1);
+	});
+
+  it('mounts a <Header /> component with user defined and verifies that clicking logout link calls logOut()', () => {
+    const value = { user: { email: 'thedude@aim.com', password: 'thedudeabides', isLoggedIn: true }, logOut: () => { } }
+    const spy = jest.spyOn(value, 'logOut');
+    const wrapper = mount(<AppContext.Provider value={value}><Header /></AppContext.Provider>);
+    wrapper.find('#logoutSection span').simulate('click');
+    expect(spy).toHaveBeenCalled();
+	});
+})
