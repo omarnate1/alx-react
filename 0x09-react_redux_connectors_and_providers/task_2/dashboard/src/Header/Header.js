28dc3d3cffaddaa5ee854a42f6e6bdee6df3f56c
omarnate1 <111249086+omarnate1@users.noreply.github.com>
2023-09-05 00:15:23 +0100

    connectors and providers

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Header/Header.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Header/Header.js
@@ -1,31 +1,78 @@
-import React from "react";
-import logo from "../assets/holberton-logo.jpg";
-import { StyleSheet, css } from "aphrodite";
-
-function Header() {
-  return (
-    <>
-      <div className={css(styles["App-header"])}>
-        <img src={logo} className={css(styles.img)} alt="logo" />
-        <h1>School dashboard</h1>
-      </div>
-    </>
-  );
-}
+import React, { cloneElement } from "react";
+import { css, StyleSheet } from 'aphrodite';
+import { connect } from 'react-redux'
+import holberton_logo from "../assets/holberton_logo.jpg";
+import { logout } from "../actions/uiActionCreators";
 
 const styles = StyleSheet.create({
-  "App-header": {
-    fontSize: "1.4rem",
-    color: "#e0354b",
+  header: {
     display: "flex",
-    alignItems: "center",
-    borderBottom: "3px solid #e0354b",
+    color: '#e14852',
+    alignItems: "center"
   },
-
-  img: {
-    width: "200px",
-    height: "200px",
+  headerLogo: {
+    width: '200px',
+    height: '200px'
   },
-});
+  span: {
+    fontStyle: 'italic',
+    textDecoration: 'underline black'
+  },
+  welcomeParagraph: {
+    marginLeft: '15px',
+    color: 'black',
+    fontSize: '1.3rem'
+  },
+  welcomeParagraphSmall: {
+    '@media (max-width: 900px)': {
+      fontSize: '1.1rem',
+      marginLeft: '10px'
+    }
+  }
+})
 
-export default Header;
+export default class Header extends React.Component {
+  
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+
+    const { user, logout } = this.props;
+
+    return (
+      <header className={css(styles.header)}>
+        <img src={holberton_logo} className={css(styles.headerLogo)} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+        { user ?
+          <p id='logoutSection' className={css(styles.welcomeParagraph, styles.welcomeParagraphSmall)}>Welcome <b>{`${user.email} `}</b><span onClick={logout} className={css(styles.span)}>(logout)</span></p>
+          : <></>
+        }
+      </header>
+
+    );
+  }
+}
+
+Header.defaultProps  = {
+  user: null,
+  logout: () => {}
+}
+
+Header.propTypes = {
+  user: propTypes.object,
+  logout: propTypes.func
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.get('user')
+  }
+}
+
+const mapDispatchToProps = () => {
+  logout
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
