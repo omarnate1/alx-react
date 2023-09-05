a4b0ecb75abab99cc5d8b4832be5e49408b92365
omarnate1 <111249086+omarnate1@users.noreply.github.com>
2023-09-05 14:51:15 +0100

    connectors and providers

--- 0x09-react_redux_connectors_and_providers/task_8/dashboard/src/selectors/notificationSelector.js
+++ 0x09-react_redux_connectors_and_providers/task_8/dashboard/src/selectors/notificationSelector.js
@@ -1,14 +1,39 @@
-export const filterTypeSelected = (data) => {
-    return data.get('filter');
-}
+import { createSelector } from "reselect";
 
-export const getNotifications = (data) => {
-    return data.get('notification')
-}
+export const filterTypeSelected = (state) => {
+  return state.get("filter");
+};
+export const getNotifications = (state) => {
+  return state.get("notifications");
+};
 
-export const getUnreadNotifications = (data) => {
-    const notification = Object.values(getNotifications(data).toJS())
-    return notification.filter((not) => {
-        not.isRead
-    })
-}
+const getNotificationsSelector = (state) => state.notifications;
+
+export const getUnreadNotificationsByType = createSelector(
+  getNotificationsSelector,
+  (notifications) => {
+    const messages = notifications.get("messages");
+    const filter = notifications.get("filter");
+
+    if (messages) {
+      let filtered;
+
+      if (filter === "URGENT") {
+        filtered = messages
+          .valueSeq()
+          .filter(
+            (value) =>
+              value.get("isRead") === false && value.get("type") === "urgent"
+          );
+      } else {
+        filtered = messages
+          .valueSeq()
+          .filter((value) => value.get("isRead") === false);
+      }
+
+      return filtered;
+    }
+
+    return messages;
+  }
+);
